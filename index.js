38ac568652776c3f59e44fa2b3a7b87a6b0316bf
krakerbrain <marioplantabaja@gmail.com>
2021-09-13 18:29:23 -0300

    Cambios en archivo index.js

--- index.js
+++ index.js
@@ -9,23 +9,24 @@ const fs = require("fs");
 
 http
   .createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+      res.end();
+    }
     axios
       .get("https://randomuser.me/api/?results=10")
       .then((response) => {
-        _.each(response.data.results, function (value, i) {
-          console.log(value.name);
-        });
-
         _.each(response.data.results, function (value, i) {
           const { first: nombre, last: apellido } = value.name;
           const id = uuidv4().slice(0, 6);
           const tiempo = moment().format("MMMM Do YYYY, h:mm:ss a");
           const datoUsuario = `${i + 1}.- Nombre: ${nombre} - Apellido: ${apellido} - ID: ${id} - Timestamp: ${tiempo}\n`;
-          //console.log(chalk.bgWhite.blue.bold(datoUsuario));
           res.write(datoUsuario);
+
           fs.appendFile("./registro.txt", datoUsuario, { encoding: "utf8", flag: "a" }, (error) => {
             if (error) throw error;
           });
+
+          console.log(chalk.bgWhite.blue.bold(datoUsuario));
         });
 
         res.end();
@@ -35,3 +36,5 @@ http
       });
   })
   .listen(8080, () => console.log("Escuchando el puerto 8080"));
+
+/*https://stackoverflow.com/questions/17952436/node-js-double-console-log-output*/
